468a3ba8a142ef6b6d11801d28578a9d713a120b
Karine Samyn <Karine.Samyn@hogent.be>
2019-10-16 16:15:18 +0200

    Add HomeController and Index View

---
+++ SnakeEyesGame/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnakeEyesGame.Models;
+
+namespace SnakeEyesGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SnakeEyes _snakeEyes;
+        public IActionResult Index()
+        {
+            _snakeEyes = new SnakeEyes();
+            return View(_snakeEyes);
+        }
+    }
+}
